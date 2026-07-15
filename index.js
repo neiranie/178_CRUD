8b90f6a94a8cfdcbb10f748f5aacd1e8674bf093
neiranie <email_anda@contoh.com>
2026-07-15 14:09:47 +0700

    Menambah endpoint DELETE /biodata/:id

--- index.js
+++ index.js
@@ -89,3 +89,21 @@ app.put('/biodata/:id', async (req, res) => {
     }
 });
 
+app.delete('/biodata/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query("DELETE FROM biodata WHERE id = $1 RETURNING *", [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Data tidak ditemukan" });
+        }
+
+        res.status(200).json({
+            message: "Berhasil menghapus data biodata",
+            data: result.rows[0]
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
+    }
+});
